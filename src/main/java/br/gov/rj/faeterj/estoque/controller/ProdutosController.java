9bf16fb261f7a1b0675739cadbfceac08bb389eb
Rubens Guimaraes <rubens.guimaraes@uol.com.br>
2020-09-14 15:50:11 -0300

    Foward/Redirect

--- src/main/java/br/gov/rj/faeterj/estoque/controller/ProdutosController.java
+++ src/main/java/br/gov/rj/faeterj/estoque/controller/ProdutosController.java
@@ -1,25 +1,36 @@
 package br.gov.rj.faeterj.estoque.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.gov.rj.faeterj.estoque.model.Produto;
 
 @Controller
 public class ProdutosController {
 
-	// Método GET
 	@RequestMapping("/produtos/novo")
 	public String novo() {
 		return "produto/CadastroProduto";
 	}
 	
-	// Método POST
 	@RequestMapping(value = "/produtos/novo", method = RequestMethod.POST)
-	public String cadastrar(Produto produto) {
+	public String cadastrar(@Valid Produto produto, BindingResult result, 
+			Model model, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			model.addAttribute("mensagem", "Erro no formulário, sku inválido!");
+			return "produto/CadastroProduto";
+		}
+		
+		// Salvar no banco de dados...
+		attributes.addFlashAttribute("mensagem", "Produto salvo com sucesso!");
 		System.out.println(">>> sku: " + produto.getSku());
-		return "produto/CadastroProduto";
+		return "redirect:/produtos/novo";
 	}
 	
 }
